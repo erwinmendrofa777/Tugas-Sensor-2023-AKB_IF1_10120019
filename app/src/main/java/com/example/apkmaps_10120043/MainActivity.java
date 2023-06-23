3c2d079caa2c3451ac0c8b8f4ccc22c75d6c0734
erwinmendrofa777 <erwinmendrofa777@gmail.com>
2023-06-23 07:42:48 +0700

    first commit

---
+++ app/src/main/java/com/example/apkmaps_10120043/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.apkmaps_10120043;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap myMap ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MainActivity.this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap){
+        myMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        MarkerOptions options = new MarkerOptions().position(sydney).title("Sydney");
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+
+        myMap.addMarker(options);
+    }
+}
